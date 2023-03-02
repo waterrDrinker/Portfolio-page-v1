fcf344a791759463136a37dd286ab6294f69df38
Grigoriy <grishatinker@gmail.com>
2023-03-02 09:54:32 +0300

    contact links and header updated

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 (function () {
   const header = document.querySelector('#header');
   window.onscroll = () => {
-    if (window.pageYOffset > 220) {
+    if (window.pageYOffset > 59) {
       header.classList.add('header_active');
     } else {
         header.classList.remove('header_active');
